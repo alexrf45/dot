021247e9a6e286c675dfd01cba4d0fe339641a52
alexrf45 <fonalex45@gmail.com>
2024-12-11 21:49:23 -0500

    moonfly

--- .config/nvim/lua/plugins/themify.lua
+++ .config/nvim/lua/plugins/themify.lua
@@ -9,14 +9,11 @@ return {
       -- Your list of colorschemes.
 
       "folke/tokyonight.nvim",
-      "Yazeed1s/minimal.nvim",
-      "sainnhe/gruvbox-material",
       "kvrohit/substrata.nvim",
-      {
-        "sho-87/kanagawa-paper.nvim",
-
-        branch = "master",
-      },
+      { "bluz71/vim-moonfly-colors", branch = "master" },
+      { "sho-87/kanagawa-paper.nvim", branch = "master" },
+      { "sainnhe/gruvbox-material", branch = "master" },
+      { "sainnhe/everforest", branch = "master" },
 
       -- Built-in colorschemes are also supported.
       -- (Also works with any colorschemes that are installed via other plugin manager, just make sure the colorscheme is loaded before Themify is loaded.)
