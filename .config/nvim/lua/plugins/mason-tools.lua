f37bcc5c59bbd472afba94e78cd15a401af6f8aa
alexrf45 <fonalex45@gmail.com>
2025-01-19 10:43:06 -0500

    mason always installed

---
+++ .config/nvim/lua/plugins/mason-tools.lua
@@ -0,0 +1,25 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "shellcheck",
+      "shfmt",
+      "flake8",
+      "ansible-language-server",
+      "bash-language-server",
+      "css-lsp",
+      "eslint-lsp",
+      "html-lsp",
+      "lua-language-server",
+      "markdown-toc",
+      "markdownlint-cli2",
+      "marksman",
+      "prettier",
+      "pylint",
+      "shfmt",
+      "stylua",
+      "tailwindcss-language-server",
+      "terraform-ls",
+    },
+  },
+}
