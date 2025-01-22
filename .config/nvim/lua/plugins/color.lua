6854615e46c86629e805c7fe5519cd78d318ff8f
alexrf45 <fonalex45@gmail.com>
2025-01-22 16:49:32 -0500

    monokai

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -1,8 +1,8 @@
 return {
-  "rebelot/kanagawa.nvim",
+  "loctvl842/monokai-pro.nvim",
   lazy = false,
   priority = 1000,
   config = function()
-    vim.cmd("colorscheme kanagawa-dragon")
+    vim.cmd("colorscheme monokai-pro-spectrum")
   end,
 }
