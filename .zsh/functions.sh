62549ebd5a3285912ec53ac33f7bf63cff9871e9
alexrf45 <fonalex45@gmail.com>
2025-04-05 17:28:07 -0400

    prompt edits

--- .zsh/functions.sh
+++ .zsh/functions.sh
@@ -1,14 +1,3 @@
-# man() {
-#   env LESS_TERMCAP_mb=$'\E[01;31m' \
-#     LESS_TERMCAP_md=$'\E[01;38;5;74m' \
-#     LESS_TERMCAP_me=$'\E[0m' \
-#     LESS_TERMCAP_se=$'\E[0m' \
-#     LESS_TERMCAP_so=$'\E[01;33;03;40m' \
-#     LESS_TERMCAP_ue=$'\E[0m' \
-#     LESS_TERMCAP_us=$'\E[04;38;5;146m' \
-#     man "$@"
-# }
-
 extract() {
   if [ -f $1 ]; then
     case $1 in
--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -3,8 +3,8 @@ function git_branch {
   git rev-parse --abbrev-ref HEAD 2> /dev/null
 }
 PS1='%F{red}${AWS_VAULT}%f '  # AWS_VAULT in cyan if set
-PS1+='%F{blue}%~%f '                       # Current directory in white
-PS1+='%F{green}$(git_branch)%f '  # Git branch
-PS1+='
-%F{green}$%f '                        # Dollar sign in green
+PS1+='[20%D %T] %n@%m %F{blue}%~%f %F{green}$(git_branch)%f 
+$ '
+
+# Dollar sign in green
 
